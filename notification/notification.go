aa45248d645f57dc80312516bb4d2df20032fd5b
Cisco Garcia <cisco@ciscogarcia.net>
2017-11-12 16:58:55 -0500

    Remove check for new notifications
    Remove Check for new notifications. These will be pulled in from the
    application when it checks for new rabbit messages, and we will just
    display the message with our static method.

--- notification/notification.go
+++ notification/notification.go
@@ -4,10 +4,6 @@ import (
 	"github.com/0xAX/notificator"
 )
 
-func CheckForNewNotifications(notifications_channel chan string) {
-
-}
-
 func DisplayNotification(title string, notification string) {
 	notify := notificator.New(notificator.Options{
 		AppName: "Notifier",
